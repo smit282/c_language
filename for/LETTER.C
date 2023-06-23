441245ed78e746bb942b9495e9d1d782c23b4831
Smit Thummar <smitthummar28@gmail.com>
2023-06-23 09:56:39 +0530

    23-6-23

--- for/LETTER.C
+++ for/LETTER.C
@@ -1,36 +1,26 @@
 #include<stdio.h>
 #include<conio.h>
-
 int main ()
-
 {
- clrscr();
-
-  char letter ;
-
-  printf("Enter a letter small or capital :  ");
-  scanf("%c",&letter);
-
-  printf("\t your letter is : %c",letter);
-
+   clrscr();
+   char letter ;
+   printf("Enter a letter small or capital :  ");
+   scanf("%c",&letter);
+   printf("\t your letter is : %c",letter);
    if((letter>=65)&&(letter<=90))
-     {
-     printf("\n\t your small letter is : %c ",letter+32);
-     printf("\n Your letter ASCII value is : %d",letter);
-     }
-
-  else if((letter>=97) &&  (letter<=122))
-      {
-      printf("\n\t your capital letter is :%c",letter-32);
-      printf("\n Your letter ASCII value is : %d",letter);
-      }
- else
    {
-   printf("\n\t [%c] your letter is not alfabate ",letter );
+       printf("\n\t your small letter is : %c ",letter+32);
+       printf("\n Your letter ASCII value is : %d",letter);
    }
-
-
-
+   else if((letter>=97) &&  (letter<=122))
+   {
+       printf("\n\t your capital letter is :%c",letter-32);
+       printf("\n Your letter ASCII value is : %d",letter);
+   }
+   else
+   {
+       printf("\n\t [%c] your letter is not alfabate ",letter );
+   }
 getch();
 }
 
